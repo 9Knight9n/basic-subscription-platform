673573f2c92f3316940a855b87ef62b699066cce
Mohamad Sajad Naghizadeh <naghizadeh.mdsd@gmail.com>
2023-05-05 01:45:13 +0330

    register models in django admin

--- django/subscription/admin.py
+++ django/subscription/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
 
+from .models import Customer, Subscription, CustomerSubscription, Invoice
+
+admin.site.register(Customer)
+admin.site.register(Subscription)
+admin.site.register(CustomerSubscription)
+admin.site.register(Invoice)
 # Register your models here.
